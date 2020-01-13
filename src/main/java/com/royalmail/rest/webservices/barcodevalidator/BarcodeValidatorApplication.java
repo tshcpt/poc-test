4d57ed3fb6d5d8f736aa80d41e02fb51f0be478a
vinoth54@gmail.com <46324726+vinoth54@users.noreply.github.com>
2020-01-13 10:34:54 +0000

    Barcode Validator

---
+++ src/main/java/com/royalmail/rest/webservices/barcodevalidator/BarcodeValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.royalmail.rest.webservices.barcodevalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcodeValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarcodeValidatorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/royalmail/rest/webservices/barcodevalidator/BarcodeValidatorServiceImpl.java
@@ -0,0 +1,61 @@
+package com.royalmail.rest.webservices.barcodevalidator;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * This Service validates the given barcode to ensure that it adheres to the
+ * business requirements. It validates the length, service indicator code,
+ * checkdigit and the country code. It also calculates the checkdigit using the
+ * input serial number to verify its value.
+ *
+ */
+@Component
+public class BarcodeValidatorServiceImpl implements BarcodeValidatorService {
+
+	/**
+	 * It validates the length, service indicator code, checkdigit and the country
+	 * code.
+	 * 
+	 * @param barcode
+	 * @return boolean
+	 */
+	public boolean validateBarcode(String barcode) {
+
+		boolean isBarcodeValid = false;
+
+		if (barcode != null && barcode.length() == BARCODELENGTH) {
+
+			String checkDigitSource = barcode.substring(2, 11);
+			String checkDigitVal = barcode.substring(10, 11);
+
+			if (barcode.matches(BARCODEEX) && checkDigitVal.equals(calculateCheckDigit(checkDigitSource))) {
+				isBarcodeValid = true;
+			}
+		}
+		return isBarcodeValid;
+	}
+
+	/**
+	 * This method calculates the checkdigit using the input serial number to verify
+	 * its value.
+	 * 
+	 * @param sourceData
+	 * @return String
+	 */
+	private String calculateCheckDigit(String sourceData) {
+		int sum = 0;
+		for (int i = 0; i < WEIGHTAGE.length; i++) {
+			sum = sum + (sourceData.charAt(i) * WEIGHTAGE[i]);
+		}
+		int checkDigit = 11 - (sum % 11);
+
+		if (checkDigit == 10)
+			return "0";
+		else if (checkDigit == 11)
+			return "5";
+		else
+			return Integer.toString(checkDigit);
+
+	}
+
+}
