2f90da6b225efd33b3d608307e98dea0d7b9ae30
Vanithas54 <38877787+Vanithas54@users.noreply.github.com>
2021-08-20 14:34:32 +0100

    Delete src directory

--- src/main/java/com/sample/rest/webservices/ledger/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sample.rest.webservices.ledger.repository;
-
-import com.sample.rest.webservices.ledger.entity.MessageDetails;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepository extends CrudRepository<MessageDetails, Long> {
-}
--- src/main/java/com/sample/rest/webservices/ledger/request/MessageHeader.java
+++
@@ -1,9 +0,0 @@
-package com.sample.rest.webservices.ledger.request;
-
-import java.util.Date;
-
-public class MessageHeader{
-    public String id;
-    public Date sentDateTime;
-    public String channelName;
-}
