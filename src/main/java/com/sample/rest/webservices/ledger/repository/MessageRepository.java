024c0c6b1ff32bb0b515d8223ff3d44d4ebd4ca8
Vanithas54 <38877787+Vanithas54@users.noreply.github.com>
2021-08-23 10:21:08 +0100

    Delete src directory

--- src/main/java/com/sample/rest/webservices/ledger/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sample.rest.webservices.ledger.repository;
-
-import com.sample.rest.webservices.ledger.entity.MessageDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<MessageDetails, Long> {
-}
